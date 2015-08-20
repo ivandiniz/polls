89165c7c23aa6643ba9801592aa8d05ae3a355af
Ivan Diniz <ivandiniz@gmail.com>
2015-08-20 18:44:21 -0300

    inicializando teste da view

--- polls/tests.py
+++ polls/tests.py
@@ -1,3 +1,42 @@
-from django.test import TestCase
+import datetime
 
-# Create your tests here.
+from django.utils import timezone
+from django.test import TestCase
+from django.core.urlresolvers import reverse
+
+from .models import Question
+
+
+class QuestionMethodTest(TestCase):
+
+    def test_was_published_recently_with_future_question(self):
+        u"""
+        was_published_recently() retorna False para perguntas que o pub_date esta no futuro
+        """
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_question = Question(pub_date=time)
+        self.assertEqual(future_question.was_published_recently(), False)
+
+    def test_was_published_recently_with_old_question(self):
+        u"""
+        was_published_recently() retorna False para perguntas que o pub_date e menor que 1 dia
+        """
+        time = timezone.now() - datetime.timedelta(days=30)
+        old_question = Question(pub_date=time)
+        self.assertEqual(old_question.was_published_recently(), False)
+
+    def test_was_published_recently_with_recent_question(self):
+        u"""
+        was_published_recently() retorna True para perguntas que o pub_date esta ate 1 dia recente
+        """
+        time = timezone.now() - datetime.timedelta(hours=1)
+        recent_question = Question(pub_date=time)
+        self.assertEqual(recent_question.was_published_recently(), True)
+
+
+def create_question(question_text, days):
+    time = timezone.now() + datetime.timedelta(days=days)
+    return Question.objects.create(question_text=question_text, pub_date=time)
+
+
+class QuestionViewTests(TestCase):
